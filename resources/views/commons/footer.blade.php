37fadf7dfb36b4da7e310c155dfd9839e85156dd
NewAuthor <newauthor@example.com>
2020-05-10 10:30:50 +0000

    laravelQUEST3 トップページと新規ユーザ登録機能の実装

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class='mt-5'>
+    <nav class="navbar navbar-light bg-light justify-content-center">
+        <div class="navbar-brand m-0">©︎ QuestAcadema, All rights reserved.</div>
+        
+    </nav>
+</footer>
