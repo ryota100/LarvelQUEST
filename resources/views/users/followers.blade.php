aae545a8fba4ef2f8b6943c69152015c5e9311b7
NewAuthor <newauthor@example.com>
2020-05-15 03:22:53 +0000

    laravelQUEST5 カラムの追加・フォロー機能の追加

---
+++ resources/views/users/followers.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('users.tabs',['user'=>$user])
+
+    @include('users.users', ['users' => $users])
+
+        
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,40 @@ Route::get('login','Auth\LoginController@showLoginForm')->name('login');
 Route::post('login','Auth\LoginController@Login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::resource('users','UsersController',['only'=>['show']]);
+Route::resource('users', 'UsersController', ['only' => ['show']]);
 
-Route::group(['middleware' => 'auth'],function(){
-    Route::resource('movies','MoviesController',['only' => ['create','store','destroy']]);
+Route::group(['prefix' => 'users/{id}'], function () {
+    Route::get('followings', 'UsersController@followings')->name('followings');
+    Route::get('followers', 'UsersController@followers')->name('followers');
+    });
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::put('users', 'UsersController@rename')->name('rename');
+    
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('unfollow');
+    });
+    
+    Route::resource('movies', 'MoviesController', ['only' => ['create', 'store', 'destroy']]);
 });
+// Route::resource('users','UsersController',['only'=>['show']]);
+
+// Route::group(['prefix'=>'users/{id}'],function(){
+//     Route::get('followings','UsersController@followings')->name('followings');
+//     Route::get('followers','UsersController@followers')->name('followers');
+    
+// });
+
+// Route::group(['middleware' => 'auth'],function(){
+//     Route::put('users','UsersController@rename')->name('rename');
+    
+//     Route::group(['prefix'=>'users/{id}'],function(){
+//         Route::post('follow','UserFollowController@store')->name('follow');
+//         Route::delete('unfollow', 'UserFollowController@destroy')->name('unfollow');
+//         // Route::delete('unfollow','UserFollowController@destroy')->name('unfollow');
+// });
+    
+    
+//     Route::resource('movies','MoviesController',['only' => ['create','store','destroy']]);
+// });
